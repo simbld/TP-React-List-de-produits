38e26ffbff5995779855dfd257c1ca96c5afb979
sim <s_bullado51@hotmail.com>
2023-11-17 06:27:31 +0100

    Initial commit vite eslint prettier json

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+        browser: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    semi: "error",
+    simpleQuote: "true",
+    "react/react-in-jsx-scope": "off",
+    "react/no-unescaped-entities": [
+      "error",
+      { forbid: [">", "}", '"', "{", "<"] },
+    ],
+  },
+};
