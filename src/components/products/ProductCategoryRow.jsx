5229835ca87ff76b13eedebcf08ee7d68b1bd5c1
sim <s_bullado51@hotmail.com>
2023-11-20 17:28:59 +0100

    Add proptypes in file for refactoring

--- src/components/products/ProductCategoryRow.jsx
+++ src/components/products/ProductCategoryRow.jsx
@@ -3,7 +3,7 @@ import { productRowPropTypes } from "../../proptypes";
 export default function ProductCategoryRow({ category }) {
     return (
         <tr className="product-category-row">
-            <td colSpan={3}>{category}</td>
+            <td colSpan={4}>{category}</td>
         </tr>
     );
 }
