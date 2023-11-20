f09cf1312dcf5c9b91185e41e16287c47cf795ea
sim <s_bullado51@hotmail.com>
2023-11-20 23:24:30 +0100

    Add props in  searchBar

--- src/components/products/SearchBar.jsx
+++ src/components/products/SearchBar.jsx
@@ -1,22 +1,31 @@
+import { searchBarPropTypes } from "../../proptypes";
 import CheckBox from "../forms/CheckBox";
 
-export default function SearchBar() {
+export default function SearchBar({
+    showStockedOnly,
+    onStockedOnlyChanged,
+    search,
+    onSearchChange,
+}) {
     return (
         <div>
             <div className="mb-3">
                 <input
                     type="text"
-                    value=""
-                    onChange={() => null}
-                    placeholder="Rechercher.."
+                    value={search}
+                    onChange={onSearchChange}
+                    placeholder="Rechercher..."
                 />
+                <input type="range" className="form-range" min={0} max={10} />
                 <CheckBox
                     id="stocked"
-                    checked={false}
-                    onChange={() => null}
+                    checked={showStockedOnly}
+                    onChange={onStockedOnlyChanged}
                     label="N'afficher que les produits en stock"
                 />
             </div>
         </div>
     );
 }
+
+SearchBar.propTypes = searchBarPropTypes;
